1425faee09685a6476c5cd254d9ec70965104023
xdc0527 <58333876+xdc0527@users.noreply.github.com>
2022-01-20 19:53:07 +0800

    Add files via upload

---
+++ bookmall-wx-api/src/main/java/org/example/bookmall/wx/web/WxIssueController.java
@@ -0,0 +1,38 @@
+package org.example.bookmall.wx.web;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.example.bookmall.db.service.LitemallIssueService;
+import org.example.bookmall.core.util.ResponseUtil;
+import org.example.bookmall.core.validator.Order;
+import org.example.bookmall.core.validator.Sort;
+import org.example.bookmall.db.domain.LitemallIssue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/wx/issue")
+@Validated
+public class WxIssueController {
+    private final Log logger = LogFactory.getLog(WxIssueController.class);
+
+    @Autowired
+    private LitemallIssueService issueService;
+
+    /**
+     * 帮助中心
+     */
+    @GetMapping("/list")
+    public Object list(String question,
+                       @RequestParam(defaultValue = "1") Integer page,
+                       @RequestParam(defaultValue = "10") Integer size,
+                       @Sort @RequestParam(defaultValue = "add_time") String sort,
+                       @Order @RequestParam(defaultValue = "desc") String order) {
+        List<LitemallIssue> issueList = issueService.querySelective(question, page, size, sort, order);
+        return ResponseUtil.okList(issueList);
+    }
+
+}
