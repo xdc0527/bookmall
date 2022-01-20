c94c2b91013802f301680b5aefb4c234782b0507
xdc0527 <58333876+xdc0527@users.noreply.github.com>
2022-01-20 19:50:10 +0800

    Add files via upload

---
+++ bookmall-core/src/test/java/org/example/bookmall/core/ExpressTest.java
@@ -0,0 +1,33 @@
+package org.example.bookmall.core;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.example.bookmall.core.express.dao.ExpressInfo;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.example.bookmall.core.express.ExpressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class ExpressTest {
+
+    private final Log logger = LogFactory.getLog(ExpressTest.class);
+    @Autowired
+    private ExpressService expressService;
+
+    @Test
+    public void test() {
+        ExpressInfo ei = null;
+        try {
+            ei = expressService.getExpressInfo("YTO", "800669400640887922");
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+        logger.info(ei);
+    }
+}
